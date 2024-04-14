cf604a8cb6485bd76cd731c8aea59584432630a1
Baglanov Alikhan <120701599+rendizi@users.noreply.github.com>
2024-04-14 04:41:37 -0900

    Updated proto file

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.4
 require (
 	github.com/MadAppGang/httplog v1.3.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	google.golang.org/grpc v1.62.1
 	google.golang.org/protobuf v1.32.0
--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"github.com/rendizi/Distributed-arithmetic-expression-evaluator/cmd/orkestrator"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	em := os.Getenv("EM")
 	log.Println(em)
 	if em == "O" {
